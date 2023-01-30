41ee2972b738803ce73691c1e193af18acf982b5
jeffsLM <jeffes.dev@gmail.com>
2023-01-30 17:12:30 -0300

    add new injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,19 @@
 import { container } from 'tsyringe';
 
-import { FoxAnimeRepository } from '../../modules/anime/implementations/FoxAnime';
+import { FoxAnimeRepository } from '../../modules/anime/implementations/FoxAnimeRepository';
 import { IFoxAnimeRepository } from '../../modules/anime/repositories/IFoxAnimeRepository';
 
+import { FoxEpisodeRepository } from '../../modules/episode/implementations/FoxEpisodeRepository';
+import { IFoxEpisodeRepository } from '../../modules/episode/repositories/IFoxEpisodeRepository';
+
+import { FoxUserHistoryRepository } from '../../modules/user_history/implementations/FoxUserHistoryRepository';
+import { IFoxUserHistoryRepository } from '../../modules/user_history/repositories/IFoxUserHistoryRepository';
+
 container.registerSingleton<IFoxAnimeRepository>('FoxAnimeRepository', FoxAnimeRepository);
+
+container.registerSingleton<IFoxEpisodeRepository>('FoxEpisodeRepository', FoxEpisodeRepository);
+
+container.registerSingleton<IFoxUserHistoryRepository>(
+  'FoxUserHistoryRepository',
+  FoxUserHistoryRepository
+);
