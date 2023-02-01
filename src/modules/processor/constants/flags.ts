dc32d5cdc0453fd6202dfd7e476a47ce4de45cea
jeffsLM <jeffes.dev@gmail.com>
2023-01-31 23:29:16 -0300

    add flags

---
+++ src/modules/processor/constants/flags.ts
@@ -0,0 +1,46 @@
+export const flags = [
+  {
+    flag: 'not_found',
+    type: [
+      {
+        variantAttemp: false,
+        message: (txt: string) => `episódio não encontrado${txt}`,
+      },
+      {
+        variantAttemp: true,
+        message: (txt: string) => `anime finalizado${txt}`,
+      },
+    ],
+  },
+  {
+    flag: 'success',
+    type: [
+      {
+        variantAttemp: false,
+        message: (txt: string) => `novo epsodio indexado${txt}`,
+      },
+    ],
+  },
+  {
+    flag: 'fail',
+    type: [
+      {
+        variantAttemp: false,
+        message: (txt: string) => `error - ${txt}`,
+      },
+    ],
+  },
+  {
+    flag: 'passthought',
+    type: [
+      {
+        variantAttemp: false,
+        message: (txt: string) => `service alterado${txt}`,
+      },
+      {
+        variantAttemp: true,
+        message: (txt: string) => `anime finalizado - sem provedor disponivel${txt}`,
+      },
+    ],
+  },
+];
