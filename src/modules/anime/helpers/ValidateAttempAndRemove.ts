de78b371d6fff6edd18939af2e9204c9b6750785
jeffsLM <jeffes.dev@gmail.com>
2023-02-01 19:42:57 -0300

    fix helpers

--- src/modules/anime/helpers/ValidateAttempAndRemove.ts
+++ src/modules/anime/helpers/ValidateAttempAndRemove.ts
@@ -1,5 +1,5 @@
 import { GetInfoAnimeByUniversalAnimeId } from './GetInfoAnimeByUniversalAnimeId';
-import { UpdateSkillController } from '../useCase/updateAnime/UpdateAnimeController';
+import { UpdateAnimeController } from '../useCase/updateAnime/UpdateAnimeController';
 
 interface IDataRequest {
   universal_anime_id: string;
@@ -9,13 +9,14 @@ class ValidateAttempAndRemove {
   async execute(data: IDataRequest): Promise<boolean> {
     const { universal_anime_id } = data;
     const getInfoAnime = new GetInfoAnimeByUniversalAnimeId();
-    const updateSkillController = new UpdateSkillController();
+    const updateAnimeController = new UpdateAnimeController();
 
     const animeData = await getInfoAnime.execute({
       universal_anime_id,
     });
 
     const lastAnimeFinded = animeData.at(-1);
+
     const lastUpdated = new Date(lastAnimeFinded.updated_at);
     const today = new Date();
     const outdated = new Date(lastUpdated.setDate(today.getDate() + 7));
@@ -23,7 +24,7 @@ class ValidateAttempAndRemove {
     const isValidToRemoveAttemp = outdated >= today;
 
     if (isValidToRemoveAttemp)
-      await updateSkillController.handle({
+      await updateAnimeController.handle({
         ...lastAnimeFinded,
         attempts_to_cancel_updates: --lastAnimeFinded.attempts_to_cancel_updates,
       });
