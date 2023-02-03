6e05d820fc7ec4b42452c50fbe8d35e764eddb7c
jeffsLM <jeffes.dev@gmail.com>
2023-02-03 00:09:35 -0300

    add charge route

--- src/routes/populate.routes.ts
+++ src/routes/populate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { PopulateByProviderQueueController } from '../modules/queue/useCase/populateByProviderQueue/PopulateByProviderQueueController';
+import { ProcessQueueByFireProviderController } from '../modules/queue/useCase/processQueueByFireProvider/ProcessQueueByFireProviderController';
 
 const populateRoutes = Router();
 
 const populateByProviderQueueController = new PopulateByProviderQueueController();
+const processQueueByFireProviderController = new ProcessQueueByFireProviderController();
 
 populateRoutes.post('/provider', populateByProviderQueueController.handle);
+populateRoutes.post('/charge', processQueueByFireProviderController.handle);
 
 export { populateRoutes };
