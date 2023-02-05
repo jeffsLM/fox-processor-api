8b0cb9c8e3d6bcb8012846ddff2e559894d1d559
jeffsLM <jeffes.dev@gmail.com>
2023-02-05 12:56:30 -0300

    fix url

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     "type": "postgres",
-    "url": 'postgress://FOX_ANIMA:je11071999@fox-anima-db.cfmsvjxqs5d0.us-east-1.rds.amazonaws.com/anima',
+    "url": process.env.DATABASE_URL,
     "migrations": ['./dist/database/migrations/*.js'],
     "entities": ['./dist/modules/**/entities/*.js'],
     "cli": {
