9bb1188112a9f7cbed797850418318445511e46f
jeffsLM <jeffes.dev@gmail.com>
2023-02-05 12:49:10 -0300

    fix envs

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     "type": "postgres",
-    "url": process.env.DATABASE_URL,
+    "url": 'postgress://FOX_ANIMA:je11071999@fox-anima-db.cfmsvjxqs5d0.us-east-1.rds.amazonaws.com/anima',
     "migrations": ['./dist/database/migrations/*.js'],
     "entities": ['./dist/modules/**/entities/*.js'],
     "cli": {
